a6c9529bb7eb4b9a101353f5dbab2dfca8c037ac
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-30 21:32:34 +0530

    Add operation done

--- LinkedListOperation.java
+++ LinkedListOperation.java
@@ -6,5 +6,11 @@ public class LinkedListOperation {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
+        System.out.println("ADD");
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.printList();
+
     }
 }
