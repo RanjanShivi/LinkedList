cf1fbf942e87a293a03efbae96d8743f34e01d38
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-30 21:22:49 +0530

    Create simple linked list

---
+++ LinkedListOperation.java
@@ -0,0 +1,10 @@
+package com;
+
+import java.util.Scanner;
+
+public class LinkedListOperation {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+    }
+}
